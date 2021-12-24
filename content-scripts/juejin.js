35bf724912178d313d672089abde1715ee8315ba
MySherry <sherryqueenlove@gmail.com>
2021-12-24 10:46:23 +0800

    Juejin Style

--- content-scripts/juejin.js
+++ content-scripts/juejin.js
@@ -7,6 +7,7 @@
   #juejin .main-container { max-width: 80vw !important}
   #juejin .main-container .main-area { width: calc(100% - 25rem - 16px)}
   #juejin .main-container .main-area .article-hero { max-width: 500px }
+  #juejin .main-container .main-area .comment-list-box { max-width: 100% }
   `;
     style.appendChild(document.createTextNode(text));
 
