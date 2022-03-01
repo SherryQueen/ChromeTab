d1c19629d65782b8cbab0a772043b2f220f355cd
MySherry <sherryqueenlove@gmail.com>
2022-03-01 14:40:26 +0800

    feat: support edge

--- extension/background.ts
+++ extension/background.ts
@@ -3,7 +3,7 @@
     if (changeInfo.status !== 'loading' || !changeInfo.url) return;
 
     let targetUrl = changeInfo.url;
-    if (targetUrl === 'chrome://newtab/') return;
+    if (['chrome://newtab/', 'edge://newtab/'].includes(targetUrl)) return;
     if (targetUrl.includes('#')) targetUrl = targetUrl.slice(0, targetUrl.indexOf('#'));
     const tabs = await chrome.tabs.query({ url: targetUrl });
     if (tabs.length > 1) {
