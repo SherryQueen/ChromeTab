577f79e1bb3e614d6b40c282c697626bec3a0ffd
MySherry <sherryqueenlove@gmail.com>
2022-03-17 19:39:31 +0800

    fix: some fix

--- extension/background.ts
+++ extension/background.ts
@@ -1,12 +1,12 @@
 (function () {
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
-    if (changeInfo.status !== 'loading' || !changeInfo.url) return;
-
+    if (!changeInfo.url) return;
     let targetUrl = changeInfo.url;
+    // cSpell: ignore newtab
     if (['chrome://newtab/', 'edge://newtab/'].includes(targetUrl)) return;
     if (targetUrl.includes('#')) targetUrl = targetUrl.slice(0, targetUrl.indexOf('#'));
     const tabs = await chrome.tabs.query({ url: targetUrl, active: false });
-    if (tabs.length > 1) {
+    if ((changeInfo.status === 'loading' && tabs.length === 1) || tabs.length > 1) {
       const targetTab = tabs.find((t) => t.id !== tabId);
       if (targetTab?.id) {
         await chrome.windows.update(targetTab.windowId, { focused: true });
