cdc8a91b328e44cbbf34b17623d31f5c5deb61c7
MySherry <mysherrymyqueen@outlook.com>
2023-03-02 19:47:30 +0800

    chore: redirect bing to global.bing

--- extension/background.ts
+++ extension/background.ts
@@ -1,7 +1,9 @@
 (function () {
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+    // 拦截重复 url
     if (!changeInfo.url) return;
     let targetUrl = changeInfo.url;
+
     // cSpell: ignore newtab
     if (['chrome://newtab/', 'edge://newtab/'].includes(targetUrl)) return;
     if (targetUrl.includes('#')) targetUrl = targetUrl.slice(0, targetUrl.indexOf('#'));
@@ -13,6 +15,21 @@
         await chrome.tabs.update(targetTab.id, { active: true, highlighted: true });
         chrome.tabs.remove(tabId);
       }
+    }
+  });
+
+  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    // 重定向必应
+    if (details.url.includes('bing.com/')) {
+      const newHostname = 'global.bing.com';
+      const obj = new URL(details.url);
+
+      const search = new URLSearchParams(obj.search ?? '');
+      search.set('setlang', 'en-us');
+      search.set('setmkt', 'en-us');
+      const newURL = 'https://' + newHostname + obj.pathname + '?' + search.toString();
+      chrome.tabs.update(details.tabId, { url: newURL });
+      return;
     }
   });
 })();
